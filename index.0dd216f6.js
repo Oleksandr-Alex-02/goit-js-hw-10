7e06e1ede40b586d03d60d4c1433307c850bc5e0
Oleksandr-Alex-02 <100072859+Oleksandr-Alex-02@users.noreply.github.com>
2022-08-15 19:43:49 +0000

    Deploying to gh-pages from @ Oleksandr-Alex-02/goit-js-hw-10@cb062ce2053f6d0056c984fc695f46077d1d9662 🚀

---
+++ index.0dd216f6.js
@@ -0,0 +1,2 @@
+!function(){var o={searcBbox:document.querySelector("#search-box"),countryList:document.querySelector(".country-list"),countryInfo:document.querySelector(".country-info")};o.searcBbox,addEventListener("input",(function(){o.countryList.textContent=o.searcBbox.value,console.log(o.searcBbox.value)})),console.log(o.searcBbox)}();
+//# sourceMappingURL=index.0dd216f6.js.map
---
+++ index.58acff54.js
@@ -0,0 +1,2 @@
+const o={searcBbox:document.querySelector("#search-box"),countryList:document.querySelector(".country-list"),countryInfo:document.querySelector(".country-info")};o.searcBbox,addEventListener("input",(function(){o.countryList.textContent=o.searcBbox.value,console.log(o.searcBbox.value)})),console.log(o.searcBbox);
+//# sourceMappingURL=index.58acff54.js.map
