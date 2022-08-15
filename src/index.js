cb062ce2053f6d0056c984fc695f46077d1d9662
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-08-15 22:42:57 +0300

    start++

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const reft = {
+  searcBbox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+reft.searcBbox, addEventListener('input', pole);
+
+console.log(reft.searcBbox);
+
+function pole() {
+  reft.countryList.textContent = reft.searcBbox.value;
+  console.log(reft.searcBbox.value);
+}
